09b5655df77d930646816fc5b75ec1eb50a520a4
Massimo Savino <north.twilight@gmail.com>
2022-08-11 20:06:00 -0700

    iLE-001b Added multiplayer view controller in storyboard and code, will refactor soon (deliberate mistake to illustrate the work)

---
+++ Code/iLE1-01-Dependency-Diagrams/iLE-001b-ComposingViewControllers/Parts1+2-StoryboardCompositionAndUnitTesting/iLE-001b-ComposingViewControllers-Composition+Testing/iLE-001b-ComposingViewControllers-Composition+Testing/VCCompositionWithStoryboards/SinglePlayerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SinglePlayerViewController.swift
+//  iLE-001b-ComposingViewControllers-Composition+Testing
+//
+//  Created by Massimo Savino on 2022-08-11.
+//
+
+import UIKit
+
+final class SinglePlayerViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var name: String? {
+        set { nameLabel?.text = newValue }
+        get { return nameLabel?.text }
+    }
+    
+    var score: String? {
+        set { scoreLabel?.text = newValue }
+        get { return scoreLabel?.text }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
