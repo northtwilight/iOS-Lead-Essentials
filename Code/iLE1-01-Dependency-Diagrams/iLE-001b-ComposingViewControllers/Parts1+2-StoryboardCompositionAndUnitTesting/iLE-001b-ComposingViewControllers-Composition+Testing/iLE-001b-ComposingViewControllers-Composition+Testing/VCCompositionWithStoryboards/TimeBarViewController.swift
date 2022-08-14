dfc357925098463df160ab2c90f3b924068d7abf
Massimo Savino <north.twilight@gmail.com>
2022-08-13 23:55:25 -0700

    iLE-001b Added TimeBar storyboard and TimeBarViewController

---
+++ Code/iLE1-01-Dependency-Diagrams/iLE-001b-ComposingViewControllers/Parts1+2-StoryboardCompositionAndUnitTesting/iLE-001b-ComposingViewControllers-Composition+Testing/iLE-001b-ComposingViewControllers-Composition+Testing/VCCompositionWithStoryboards/TimeBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TimeBarViewController.swift
+//  iLE-001b-ComposingViewControllers-Composition+Testing
+//
+//  Created by Massimo Savino on 2022-08-13.
+//
+
+import UIKit
+
+final class TimeBarViewController: UIViewController {
+    @IBOutlet private weak var barView: UIView?
+    
+    var progress: Float = 1 {
+        didSet { /** update bar frame */ }
+    }
+}
