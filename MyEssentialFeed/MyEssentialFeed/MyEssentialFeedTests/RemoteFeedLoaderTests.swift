1a841b2abb95c6f2969c754d0e50d0fd9fc42efa
Massimo Savino <north.twilight@gmail.com>
2022-12-05 01:00:55 -0800

    Make HTTP Client a protocol

--- MyEssentialFeed/MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
+++ MyEssentialFeed/MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,20 +7,12 @@
 
 import XCTest
 
-class HTTPClient {
-    // Refactoring Step 1:
-    //  Make the shared instance a variable and not a let statement
-    static var shared = HTTPClient()
-    
-    // Refactoring Step 2:
-    //  Move the test logic from the RemoteFeedLoader to HTTPClient VVVVVVVVV
-    func get(from url: URL) {}
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
-    // Refactoring Step 3:
-    //  Move the test logic to a new subclass of the HTTPClient
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
     
@@ -29,10 +21,17 @@ class HTTPClientSpy: HTTPClient {
 
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        // Refactoring Step 2:
-        //  Move the test logic from the RemoteFeedLoader to HTTPClient ^^^^^^
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        let originalUrlString = "https://a-url.com"
+        let urlString = "https://slashdot.org"
+        
+        client.get(from: URL(string: originalUrlString)!)
     }
 }
 
@@ -47,27 +46,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func test_init_doesNotRequestDataFromURL() {
-        // Now we no longer have a singleton and the test logic is now in a testable type, the client spy.
-        
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
+        let sut = RemoteFeedLoader(client: client)
         
-        let sut = RemoteFeedLoader()
-        sut.load()
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
-        // Refactoring step 4:
-        //  Swap the HTTPClient shared instance with the spy subclass during tests.
-        
-        
         // Arrange
         //  Given a client and sut
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         // Act
         //  When we invoke sut.load,
@@ -76,7 +65,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         // Assert
         // Then assert that a URL request was initiated in the client)
         
-        XCTAssertNotNil(client.requestedURL) 
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
